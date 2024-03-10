56b38dfdfa5b25911b1e2680cac6cc5137fe9188
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-03-09 19:25:00 -0600

    working on the exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book Club!";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1678290705i/123206298.jpg";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSijda_tts9zRstuDDRkDsh-1OmLp0sdRRtlw&usqp=CAU";
+   let book3 = "https://img.buzzfeed.com/buzzfeed-static/static/2020-10/6/19/asset/a47d43dbc642/sub-buzz-10116-1602012920-16.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Murtagh by Christopher Paolini" />
+         <img src={book2} alt="Inheritance by Nora Roberts" />
+         <img src={book3} alt="Broken Wish by Julie C. Dao" />
       </div>      
    );
 }
