73816ec96d001bfc890d3c550bb0e65b1ab4466a
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-03-14 15:17:51 -0500

    Part 2 exercises complete

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,24 @@
-import './styles.css';
-import Button from './Button';
+import "./styles.css";
+import Button from "./Button";
+import oceans from "./oceans.json";
 
-
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
-   );
+const listItem = oceans.map((ocean) => (
+  <div key={ocean.id} className={`${ocean.fishCheck === "true" ? 'isAFish':'profile'}`}>
+    <img src={ocean.image} alt={ocean.name} className="img" />
+    <h1>{ocean.name}</h1>
+    <h2>Fun Facts!</h2>
+    <ul>
+      <li>{ocean.fact1}</li>
+      <li>{ocean.fact2}</li>
+      <li>{ocean.fact3}</li>
+    </ul>
+    <Button />
+  </div>
+));
+function Profile() {
+  return (
+      <ul>{listItem}</ul>
+  );
 }
 
 export default Profile;
